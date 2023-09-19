6e305e078f538f30eb20fbc097ceca4be7cc6134
maaz54 <47806802+maaz54@users.noreply.github.com>
2023-09-19 18:18:01 +0500

    feat: camera work

---
+++ CyberSpeed-Test/Assets/GameAssets/Scripts/Gameplay/Camera/CameraSize.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using Gameplay.Interface;
+
+namespace Gameplay
+{
+    public class CameraSize : MonoBehaviour
+    {
+        [SerializeField] Transform[] cards;
+
+        private void Start()
+        {
+            GameplayManager.Instance.OnCardsGenerated += OnCardsGenerate;
+        }
+
+        private void OnCardsGenerate(ICard[,] cards)
+        {
+
+            this.cards = cards.Cast<ICard>().Select(card => card.Transform).ToArray();
+
+            Vector3 centerPosition = CalculateCenterPosition();
+
+            Camera.main.transform.position = new Vector3(centerPosition.x, centerPosition.y, Camera.main.transform.position.z);
+            Camera.main.orthographicSize = (cards.GetLength(0) > cards.GetLength(1)) ? cards.GetLength(0) : cards.GetLength(1);
+        }
+
+        Vector3 CalculateCenterPosition()
+        {
+            Vector3 sum = Vector3.zero;
+
+            foreach (Transform tile in cards)
+            {
+                sum += tile.position;
+            }
+
+            Vector3 centerPosition = sum / cards.Length;
+
+            return centerPosition;
+        }
+    }
+}
